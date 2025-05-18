075059fe10e6084f0cbb740211f7f2e2b7dc0536
Ashley <panelo.ashley@gmail.com>
2025-05-18 20:59:05 +0800

    Add project files.

---
+++ SampleQuizApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SampleQuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is 1+1? ");
+            Console.WriteLine("1: 1\n2: 2\n3: 3\n4: 4");
+            int userAnswer = int.Parse(Console.ReadLine());
+
+            Question question1 = new Question("what is 1+1?", [ "1", "2", "3", "4" ], 1);
+
+            if (question1.CheckAnswer(userAnswer))
+            {
+                Console.WriteLine("Correct!");
+            }
+            else {
+                Console.WriteLine($"Wrong! The Correct answer is: {question1.Answers[question1.CorrectAnswerIndex]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SampleQuizApp/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleQuizApp
+{
+    class Question
+    {
+        public string QuestionText { get; set; }
+
+        public string[] Answers { get; set; }
+
+        public int CorrectAnswerIndex { get; set; }
+
+        public Question(string question, string[] choices, int CorrectAnswer) {
+
+            QuestionText = question;
+            Answers = choices;
+            CorrectAnswerIndex = CorrectAnswer;
+        }
+
+        public bool CheckAnswer(int userChoice) {
+            userChoice -= 1;
+            return CorrectAnswerIndex == userChoice;
+        }
+        
+        
+    }
+}
